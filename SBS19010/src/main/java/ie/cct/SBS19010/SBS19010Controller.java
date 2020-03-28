48279b12e32728c92c78de7282a01a9ebf2c3190
spjjdev <spjjdev@gmail.com>
2020-03-28 21:48:04 +0000

    getters,setters,constructors in animal

--- SBS19010/src/main/java/ie/cct/SBS19010/SBS19010Controller.java
+++ SBS19010/src/main/java/ie/cct/SBS19010/SBS19010Controller.java
@@ -1,15 +1,24 @@
 package ie.cct.SBS19010;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //this is the controller class, the following annotation tells spring this is a controller
 @RestController
 public class SBS19010Controller {
+	
+	
 
 	//this annotation connects the method to the HTTP resource
 	@GetMapping("hello-world")
 	public String hello() {
 		return "hello";
+	}
+	
+	//this annotation is for receiving data from the client, creating an instance of an animal
+	@PostMapping("add-animal")
+	public String addAnimal() {
+		return "test";
 	}
 }
