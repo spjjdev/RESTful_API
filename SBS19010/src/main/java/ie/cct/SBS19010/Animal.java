6620fcada570d807f24ddb5404c290bf8c8076a6
spjjdev <spjjdev@gmail.com>
2020-03-29 21:18:19 +0100

    endpoints
    creation of endpoints: average weight and sellable animals

--- SBS19010/src/main/java/ie/cct/SBS19010/Animal.java
+++ SBS19010/src/main/java/ie/cct/SBS19010/Animal.java
@@ -5,4 +5,33 @@ public class Animal {
 	private String type;
 	private double weight;
 	private double price;
+	
+	//Without this default constructor spring will not create the object through JSON 
+	//because it wont use the overloaded constructor below.This allows spring to build an empty object
+	public Animal () {}
+	
+	public Animal(String type, double weight, double price) {
+		super();
+		this.type = type;
+		this.weight = weight;
+		this.price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
--- SBS19010/src/main/java/ie/cct/SBS19010/SBS19010Controller.java
+++ SBS19010/src/main/java/ie/cct/SBS19010/SBS19010Controller.java
@@ -1,24 +1,66 @@
 package ie.cct.SBS19010;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //this is the controller class, the following annotation tells spring this is a controller
 @RestController
 public class SBS19010Controller {
-	
-	
 
-	//this annotation connects the method to the HTTP resource
+	private List<Animal> animals;
+
+	public SBS19010Controller() {
+		animals = new ArrayList<Animal>();
+		animals.add(new Animal("Cow", 300, 500));
+		animals.add(new Animal("Pig", 100, 250));
+		animals.add(new Animal("Chicken", 0.5, 5));
+	}
+
+	// this annotation connects the method to the HTTP resource
 	@GetMapping("hello-world")
 	public String hello() {
 		return "hello";
 	}
-	
-	//this annotation is for receiving data from the client, creating an instance of an animal
+
+	// this annotation is for receiving data from the client, creating an instance
+	// of an animal
+	// the RequestBody annotation means that we will receive info from the body and
+	// not the URL
 	@PostMapping("add-animal")
-	public String addAnimal() {
-		return "test";
+	public List<Animal> addAnimal(@RequestBody Animal animal) {
+		animals.add(animal);
+		return animals;
 	}
+
+	@GetMapping("average-weight")
+	public double averageWeight() {
+		double avgWeight = 0;
+		for (Animal animal : animals) {
+			avgWeight += animal.getWeight();
+		}
+		avgWeight = avgWeight / animals.size();
+		// create an object to return JSON instead of double
+		return avgWeight;
+	}
+
+	@GetMapping("sellable-animals")
+	public Map<String, Integer> getSellableAnimals() {
+		Map<String, Integer> summary = new  HashMap <String, Integer>();
+		for (Animal animal : animals) {
+			if (summary.get(animal.getType())==null) {
+				summary.put(animal.getType(),1);
+			} else {
+				Integer count = summary.get(animal.getType());
+				summary.put(animal.getType(),++count);
+			}
+		} return summary;
+	}
+
 }
