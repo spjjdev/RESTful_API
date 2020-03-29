c78eb20a117f272132b486862b31eeef88496e32
spjjdev <spjjdev@gmail.com>
2020-03-29 22:56:46 +0100

    first version of 5th endpoint
    version that throws 500 exception

--- SBS19010/src/main/java/ie/cct/SBS19010/SBS19010Controller.java
+++ SBS19010/src/main/java/ie/cct/SBS19010/SBS19010Controller.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //this is the controller class, the following annotation tells spring this is a controller
@@ -52,15 +53,34 @@ public class SBS19010Controller {
 
 	@GetMapping("sellable-animals")
 	public Map<String, Integer> getSellableAnimals() {
-		Map<String, Integer> summary = new  HashMap <String, Integer>();
+		Map<String, Integer> summary = new HashMap<String, Integer>();
 		for (Animal animal : animals) {
-			if (summary.get(animal.getType())==null) {
-				summary.put(animal.getType(),1);
+			if (summary.get(animal.getType()) == null) {
+				summary.put(animal.getType(), 1);
 			} else {
 				Integer count = summary.get(animal.getType());
-				summary.put(animal.getType(),++count);
+				summary.put(animal.getType(), ++count);
 			}
-		} return summary;
+		}
+		return summary;
+	}
+
+	// using requestparam because the url by default doesnt need a parameter but we
+	// will make it so
+	@GetMapping("farm-value")
+	public double totalValueOfAnimals(@RequestParam(required = true) String animalType) {
+		// this currently only returns the total price of one kind of animal,need to be
+		// all animals
+		double valueOfAnimals = 0;
+		for (Animal animal : animals) {
+			if (animal.getType().contentEquals(animalType)) {
+				valueOfAnimals += animal.getPrice();
+			}
+		}
+		if(valueOfAnimals == 0) {
+			throw new RuntimeException ("There is no " + animalType + "/s on the farm");
+		}
+		return valueOfAnimals;
 	}
 
 }
